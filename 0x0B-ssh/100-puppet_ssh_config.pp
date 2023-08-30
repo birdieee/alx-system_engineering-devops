b539044299049f093737bb02f43ca8c9a5421411
birdieee <lolasimbiat@gmail.com>
2023-08-30 12:28:42 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# usig puppet to make changes to our configuration file
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration 
+	host*
+	IdentifyFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
